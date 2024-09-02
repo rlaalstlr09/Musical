23d688596b2dea275dc48c67f108b94ba040f5ba
adventureJava <poker6711@naver.com>
2024-09-02 09:16:15 +0900

    dd

--- MusicalProject/src/main/java/com/human/service/QaServiceImpl.java
+++ MusicalProject/src/main/java/com/human/service/QaServiceImpl.java
@@ -7,8 +7,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
+import com.human.dao.IQaDao;
 import com.human.dto.QaDto;
 
 import com.human.vo.BoardVo;
@@ -21,23 +20,23 @@ public class QaServiceImpl implements IQaService {
 	
 	
 	public List<QaDto> qa_listSearch(BoardVo vo) throws Exception{
-		IQaService dao = sqlSession.getMapper(IQaService.class);
+		IQaDao dao = sqlSession.getMapper(IQaDao.class);
 		return dao.qa_listSearch(vo);
 	}
 	public int qa_listSearchCount(BoardVo vo) throws Exception{
-		IQaService dao = sqlSession.getMapper(IQaService.class);
+		IQaDao dao = sqlSession.getMapper(IQaDao.class);
 		return dao.qa_listSearchCount(vo);
 	}
 	public void res_update(Integer qa_id,String response,Integer display) throws Exception{
-		IQaService dao = sqlSession.getMapper(IQaService.class);
+		IQaDao dao = sqlSession.getMapper(IQaDao.class);
 		dao.res_update(qa_id,response,display);
 	}
 	public List<QaDto> qa_list(BoardVo vo,String customer_id) throws Exception{
-		IQaService dao = sqlSession.getMapper(IQaService.class);
+		IQaDao dao = sqlSession.getMapper(IQaDao.class);
 		return dao.qa_list(vo,customer_id);
 	}
 	public int qa_listCount(String customer_id) throws Exception{
-		IQaService dao = sqlSession.getMapper(IQaService.class);
+		IQaDao dao = sqlSession.getMapper(IQaDao.class);
 		return dao.qa_listCount(customer_id);
 	}
 }
