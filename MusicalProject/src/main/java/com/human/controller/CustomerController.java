8ddf6073b2ae3496dc8b60dee6b572642de505a9
DESKTOP-6N29NFS\tj-bu-702-13 <lshstory0805@naver.com>
2024-10-18 11:59:12 +0900

    문자 인증 추가

--- MusicalProject/src/main/java/com/human/controller/CustomerController.java
+++ MusicalProject/src/main/java/com/human/controller/CustomerController.java
@@ -2,6 +2,7 @@
 package com.human.controller;
 
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.json.simple.JSONObject;
 //import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +30,9 @@ import com.human.dto.CustomerDto;
 import com.human.service.IAuthoritiesService;
 import com.human.service.ICustomerService;
 import com.human.service.MailService;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 // 로그인 관련 작업 처리 컨트롤러
 @Controller
@@ -55,6 +60,40 @@ public class CustomerController {
 		System.out.print("메일 전송 완료");
 		return "customer/search_id";
 	}
+	@RequestMapping(value = "customer/sendSms.do", method = RequestMethod.POST)
+	public String sendSimpleSms(CustomerDto dto,HttpServletRequest request,HttpSession session) throws Exception{
+		String auth_code = mailService.createNumber(); // 랜덤 인증코드
+		session.setAttribute("authCode", auth_code);
+		session.setMaxInactiveInterval(60); // 인증코드 유호시간 1분
+		System.out.println(session.getAttribute("authCode"));
+		System.out.println("sms메서드 실행");
+		String api_key = ""; //API KEY 발급받은것 입력
+		String api_secret = ""; //API KEY 발급받은것 입력
+		Message coolsms = new Message(api_key, api_secret);
+		
+		HashMap<String, String> set = new HashMap<String, String>();
+		System.out.println("customer_phone:"+dto.getCustomer_phone());
+		set.put("to",dto.getCustomer_phone()); //수신번호
+		
+		String  messageText = "[티켓보라] 인증 코드는" + auth_code + "입니다.";
+		if(messageText == null || messageText.isEmpty()) {
+			throw new IllegalArgumentException("문자 내용은 필수입니다.");
+		}
+		
+		set.put("from", "발신번호 입력"); //발신번호
+		set.put("text", messageText); //문자 내용
+		set.put("type", "sms"); // 문자 타입
+		set.put("app_version", "test app 1.2");
+
+		System.out.println(set);
+		try {
+			JSONObject result = coolsms.send(set); // 보내기&전송결과받기
+		} catch(CoolsmsException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCode());
+		}
+		return "customer/search_id";
+	}
 	
 	
 	@RequestMapping(value = "customer/insert", method = RequestMethod.POST)
@@ -76,7 +115,7 @@ public class CustomerController {
 			return "customer/error";
 		}
 	}
-	
+	// 아이디 찾기(이메일)
 	@RequestMapping(value = "/customer/findIdCheck", method = RequestMethod.POST)
 	public String searchId(@RequestParam("auth_code") String auth_code, HttpSession session,HttpServletRequest request, Model model, CustomerDto dto, @RequestParam("customer_email") String customer_email) throws Exception{
 		try {
@@ -99,7 +138,32 @@ public class CustomerController {
 		}
 		return "customer/search_id";
 	}
+	// 아이디 찾기(문자)
+	@RequestMapping(value = "/customer/findIdSmsCheck", method = RequestMethod.POST)
+	public String searchIdPhone(@RequestParam("auth_code") String auth_code, HttpSession session,HttpServletRequest request, Model model, CustomerDto dto, @RequestParam("customer_phone") String customer_phone) throws Exception{
+		try {
+			String code = (String) session.getAttribute("authCode");
+			System.out.println("문자인증");
+			System.out.println("auth_code:"+auth_code);
+			System.out.println("code:"+code);
+			boolean isMatch = auth_code.equals(String.valueOf(code));
+			
+			if(isMatch) {
+				System.out.println("인증 완료");
+				request.setAttribute("msg", "인증 성공");
+				dto.setCustomer_phone(customer_phone);
+				List<CustomerDto> customer_id = customerService.findCustomerIdPhone(customer_phone);
+				model.addAttribute("findId",customer_id);
+				return "customer/findIdResult";
+			}	
+		} catch(Exception e) {
+			model.addAttribute("msg", "오류가 발생되었습니다.");
+			e.printStackTrace();
+		}
+		return "customer/search_id";
+	}
 	
+	// 비번 찾기(이메일)
 	@RequestMapping(value = "/customer/findPwCheck", method = RequestMethod.POST)
 	public String searchPw(@RequestParam("auth_code") String auth_code, HttpSession session,HttpServletRequest request, Model model, CustomerDto dto, @RequestParam("customer_id") String customer_id, @RequestParam("customer_email") String customer_email) throws Exception{
 		System.out.println("try실행 오류");
@@ -123,6 +187,42 @@ public class CustomerController {
 					String newPw = RandomStringUtils.randomAlphanumeric(10);
 					dto.setCustomer_pw(newPw);
 					customerService.newPwUpdate(dto);
+					model.addAttribute("newPwd",newPw);
+					return "customer/findPwResult";
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("msg","오류가 발생되었습니다.");
+			return "customer/search_pw";
+		}
+		return "customer/search_pw";
+	}
+	
+	// 비번 찾기(문자)
+	@RequestMapping(value = "/customer/findPwSmsCheck", method = RequestMethod.POST)
+	public String searchPwPhone(@RequestParam("auth_code") String auth_code, HttpSession session,HttpServletRequest request, Model model, CustomerDto dto, @RequestParam("customer_id") String customer_id, @RequestParam("customer_phone") String customer_phone) throws Exception{
+		System.out.println("try실행 오류");
+		try {
+			String code = (String) session.getAttribute("authCode");
+			System.out.println("auth_code:"+auth_code);
+			System.out.println("code:"+code);
+			boolean isMatch = auth_code.equals(String.valueOf(code));
+			
+			if(isMatch) {
+				dto.setCustomer_id(customer_id);
+				dto.setCustomer_phone(customer_phone);
+				int search = customerService.pwCheckPhone(dto);
+				System.out.println("유효성 검사 시작");
+				if(search == 0) {
+					System.out.println("정보 오류");
+					model.addAttribute("msg","기입된 정보가 잘못되었습니다. 다시 입력해주세요.");
+					return "customer/search_pw";
+				}
+					System.out.println("정상 작동");
+					String newPw = RandomStringUtils.randomAlphanumeric(10);
+					dto.setCustomer_pw(newPw);
+					customerService.newPwUpdatePhone(dto);
 					model.addAttribute("newPwd",newPw);
 					return "customer/findPwResult";
 			}
--- MusicalProject/src/main/webapp/WEB-INF/views/customer/search_pw.jsp
+++ MusicalProject/src/main/webapp/WEB-INF/views/customer/search_pw.jsp
@@ -101,9 +101,36 @@ p {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 <script>
 $(document).ready(function() {
+	$("input[name='method']").change(function(){
+		$("input[name='method']").not(this).prop('checked',false);
+		console.log('메서드 선택');
+		if($(".box1").is(':checked')){
+			console.log("문자인증");
+			$('.cd1').prop('type','text');
+			$('#customer_phone').prop('type','text');
+			$('#customer_email').prop('type','hidden');
+		}else{
+			$('.cd1').prop('type','hidden');
+			$('#customer_phone').prop('type','hidden');
+		}
+		if($(".box2").is(':checked')){
+			$('.cd2').prop('type','text');
+			$('#customer_email').prop('type','text');
+			$('.cd1').prop('type','hidden');
+			$('#customer_phone').prop('type','hidden');
+		}else{
+			$('.cd2').prop('type','hidden');
+			$('#customer_email').prop('type','hidden');
+		}
+	})
+	
 	// 이메일 인증 코드 발송 함수
 	window.sendAuthCode = function() {
+		var method = $("input[name='method']:checked").val();
 		var email = $("#customer_email").val();
+		var phone = $("#customer_phone").val();
+		
+		if(method === "email"){
 		if (!email) {
 			alert("이메일을 입력해주세요.");
 			return;
@@ -114,12 +141,50 @@ $(document).ready(function() {
 			customer_email: email
 		}, function(result) {
 			alert("인증 코드가 발송되었습니다.");
-			$("#auth_code").prop('type', 'text'); // 숨겨진 인증 코드 필드 활성화
+			$("#auth_code2").prop('type', 'text'); // 숨겨진 인증 코드 필드 활성화
 		}).fail(function() {
 			alert("이메일 발송 오류");
 		});
+		} else if(method === "sms"){
+			if(!phone){
+				alert("전화번호를 입력해주세요.");
+				return;
+			}
+			// 문자로 인증 코드 발송 요청
+			$.post("sendSms.do",{customer_phone: phone},function(result){
+				alert("인증 코드가 발송되었습니다.");
+				$("#auth_code1").prop('type','text');
+			}).fail(function(){
+				alert("문자 발송 오류");
+			})
+		}else{
+			alert("방법을 선택해주세요.");
+		}
 	}
 
+	window.submitForm = function(){
+		var method = $("input[name='method']:checked").val();
+		if(!method){
+			alert("인증 방법을 선택해주세요.");
+			return;
+		}
+		
+		var authCode;
+		
+		if(method === "sms"){
+			authCode = $("#auth_code1").val();
+		}else if(method === "email"){
+			authCode = $("#auth_code2").val();
+		}
+		
+		if(!authCode){
+			alert("인증 코드를 입력해주세요.");
+			return;
+		}
+		var formId = method === "sms" ? "smsForm" : "findForm";
+		$("#" + formId).submit();
+	}
+	
 	// 인증 코드 확인 함수
 	window.checkAuthCode = function(event) {
 		event.preventDefault();  // 기본 폼 제출 동작을 막음
@@ -130,19 +195,6 @@ $(document).ready(function() {
 			return;
 		}
 
-/* 		// 서버로 인증 코드 확인 요청
-		$.post("codeCheck", {
-			customer_code: authCode
-		}, function(result) {
-			if (result.includes("인증 성공")) {
-				alert("인증에 성공했습니다.");
-				$("#findForm").off('submit').submit();  // 인증 성공 후 폼 제출
-			} else {
-				alert("인증 코드가 잘못되었습니다.");
-			}
-		}).fail(function() {
-			alert("인증 코드 확인 오류");
-		}); */
 	}
 });
 </script>
@@ -153,20 +205,32 @@ $(document).ready(function() {
 		<c:if test="${not empty msg }">
 			<p>${msg }</p>
 		</c:if>
-		<form id="findForm" action="findPwCheck" method="post">
-			<div>
-				<input type="text" name="customer_id" id="customer_id"
-					placeholder="아이디">
-			</div>
+		<form id="smsForm" action="findPwSmsCheck" method="post" >
 			<div class="form-group">
-				<input type="email" name="customer_email" id="customer_email"
+				<label><input type="checkbox" name="method" value="sms" id="box" class="box1">
+				<span class="text">문자 인증</span></label>
+				<input type="hidden" name="customer_id" id="customer_id" class="cd1"
+					placeholder="아이디">
+				<input type="hidden" name="customer_phone" id="customer_phone"
+					placeholder="전화번호">
+				<input type="hidden" name="auth_code" id="auth_code1" class="auth_code"
+					placeholder="인증 코드입력">
+			</div>
+		</form>
+		<form id="findForm" action="findPwCheck" method="post">
+			<div class="form-group">
+				<label><input type="checkbox" name="method" value="email" id="box" class="box2">
+				<span class="text">이메일 인증</span></label>
+				<input type="hidden" name="customer_id" id="customer_id" class="cd2"
+					placeholder="아이디">
+				<input type="hidden" name="customer_email" id="customer_email"
 					placeholder="이메일">
-				<input type="hidden" name="auth_code" id="auth_code" placeholder="인증 코드입력">
-				
+				<input type="hidden" name="auth_code" id="auth_code2" class="auth_code"
+					placeholder="인증 코드입력">
 			</div>
 			<input type="button" id="send_auth" onclick="sendAuthCode();" value="인증 코드발송">
-			<input type="submit" id="pw-find" value="비밀번호 찾기">
 		</form>
+		<input type="button" id="pw-find" onclick="submitForm()" value="비밀번호 찾기">
 	</div>
 </body>
 </html>
